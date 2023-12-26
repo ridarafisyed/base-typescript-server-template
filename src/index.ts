be614c956a6ff81e9e249b641cc5e48b90c6df4c
AURTeck <112095879+aurteck-codehub@users.noreply.github.com>
2023-12-26 11:58:10 +0500

    update the server with cookies body parser compression and cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import express from "express"
+import http from "node:http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser"
+import compression from "compression"
+import cors from "cors"
 
 const app = express()
-
 const hostname = "127.0.0.1";
-const port = 8000;
+const port = 8080;
+
+
+app.use(cors({
+    credentials:true,
+}))
+
+app.use(compression())
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+
+const server = http.createServer(app)
 
 app.get("/", (req, res) => { res.send("Hello World") })
 
-app.listen(port, hostname, () => {
-    console.log(`App is running at http://${hostname}:${port}/`)
-})
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
